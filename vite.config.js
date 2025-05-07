ce3085ec12fbee25b56fe7ad7ffd205a8260ee7a
aratislamsafol <aratislamsafol@gmail.com>
2025-05-07 19:09:00 +0600

    Chunk was vast for package so minified and rewrite vite.config file, and live link added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          icons: ['react-icons'],
+          charts: ['recharts'],
+          toast: ['react-toastify'],
+        },
+      },
+    },
+  },
 })
